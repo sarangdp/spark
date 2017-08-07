3f914111a9bc0f9fe8f15326138422a01b104d59
Sarang Panchawadkar <sarangdp@gmail.com>
2017-08-07 15:49:02 -0400

    Create SparkStreamingWordCount.scala

---
+++ streaming/SparkStreamingWordCount.scala
@@ -0,0 +1,25 @@
+package KafkaInt
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming._
+
+/**
+  * Created by Sarang on 07-08-2017.
+  */
+object SparkStreamingWordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Testing streaming").setMaster("yarn-client");
+    val ssc = new StreamingContext(conf, Seconds(10));
+
+    val lines = ssc.socketTextStream("gw01.it.com", 15151)
+    val linesFlatMap = lines.flatMap(rec => rec.split(" "));
+    val linesMap = linesFlatMap.map((_, 1));
+    val lineRBK = linesMap.reduceByKey(_+_);
+    lineRBK.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
